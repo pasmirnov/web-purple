814fa3d1651f2ead26469f1598586d5ea1a2089f
Pavel Smirnov <pavel_smirnov@epam.com>
2017-04-14 15:03:15 +0300

    :construction: Add stubs for posts

---
+++ src/components/feed-page/posts-stub.js
@@ -0,0 +1,59 @@
+const author =
+    {
+        displayName: 'Andrey Semin',
+        avatar: 'https://pp.userapi.com/c604521/v604521206/36b42/5NxkjLHfsIs.jpg'
+    };
+
+export const tags = [
+    'Angular',
+    'HTML',
+    'Front-end',
+    'CSS',
+    'Community',
+];
+export default [
+    {
+        title: 'Preload: What Is It Good For? – Smashing Magazine',
+        description: `Preload is a new web standard for improving performance.
+         It gives developers the ability to define custom loading logic.`,
+        url: 'https://www.smashingmagazine.com/2016/02/preload-what-is-it-good-for/',
+        image: 'https://www.smashingmagazine.com/wp-content/uploads/2015/11/skeleton-500px-opt.png',
+        date: new Date(),
+        original: true,
+        tags,
+        author,
+    },
+    {
+        url: 'https://medium.com/javascript-and-opinions/redux-side-effects-and-you-66f2e0842fc3#.pgsg9zytu',
+        title: 'Redux side effects and you — JavaScript and Opinions',
+        description: 'Should you care about alternatives to the venerable redux-thunk?',
+        image: 'https://cdn-images-1.medium.com/max/1200/1*_zUh7Lutwfa5WcsZaF4SeA.png',
+        date: new Date(),
+        original: false,
+        tags,
+        author,
+    },
+    {
+        url: 'https://youtu.be/9QveBbn7t_c',
+        title: 'Monad - FunFunFunction #21',
+        description: `Monads is a type of Functor, so you need to know what a Functor is in order for this episode to make sense.
+         Thankfully, there there is a FunFunFunction episo...`,
+        image: 'https://i.ytimg.com/vi/9QveBbn7t_c/maxresdefault.jpg',
+        date: new Date(),
+        original: false,
+        tags,
+        author,
+    },
+    {
+        url: 'https://medium.com/personal-growth/tell-me-where-it-hurts-fa09210c7d0a#.mtq0xljcg',
+        title: 'Tell Me Where It Hurts – Personal Growth',
+        description: 'On imperfection and revelation',
+        comment: 'https://medium.com/personal-growth/tell-me-where-it-hurts-fa09210c7d0a#.mtq0xljcg',
+        image: 'https://cdn-images-1.medium.com/max/1200/1*a-pCxPSdw7_7RZEcOdfpQQ.jpeg',
+        date: new Date(),
+        original: true,
+        tags,
+        author,
+    },
+];
+
