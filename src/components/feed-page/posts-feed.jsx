189c65af0ea00ca440adc045df370a5c91238c38
Pavel Smirnov <pavel_smirnov@epam.com>
2017-08-10 16:20:43 +0300

    :recycle: extract PostsList component into dedicated file

--- src/components/feed-page/posts-feed.jsx
+++ src/components/feed-page/posts-feed.jsx
@@ -1,102 +1,19 @@
 import React from 'react';
 import styled, { withTheme } from 'styled-components';
 
-import posts, { tags } from './posts-stub';
+import { tags } from './posts-stub';
+import BlockHeader from '../common/block-header';
+import MainContainer from '../common/main-container';
 
 import {
     FilterBlock,
     Search,
 } from '../page-filter';
 
+import PostsList from './posts-list';
+
 import { TagList } from '../common/tag';
-import { media } from '../../utils/css-utils';
-import MainContainer from '../common/main-container';
-import BlockHeader from '../common/block-header';
-import { WebpurpleStampIcon } from '../icons';
 
-const Post = styled.li`
-    position: relative;
-    width: 100%;
-    ${media.tablet`width: 30rem;`}
-    ${media.hd`width: 35rem;`}
-    padding: 2.5rem;
-    margin-bottom: 2rem;
-    ${media.desktop`margin-bottom: 7rem;`}
-    box-sizing: border-box;
-    overflow: hidden;
-    box-shadow: 0 0 8px 1px #bbb;
-`;
-
-const PostsList = styled.ul`
-    list-style: none;
-    padding: 0;
-    margin: 3.6rem 0 0;
-    ${media.desktop`margin-top: 10rem;`}
-    display: flex;
-    flex-wrap: wrap;
-    justify-content: space-between;
-`;
-const BackgroundShape = styled.div`
-    position: absolute;
-    top: 0;
-    left: -60%;
-    width: 230%;
-    height: 100%;
-    transform: skew(-60deg, 0);
-    overflow: hidden;
-    z-index: -1;
-`;
-
-const BackgroundImage = styled.div`
-    width: 100%;
-    height: 100%;
-    filter: grayscale(100);
-    opacity: .15;
-    background: url(${props => props.url});
-    background-repeat: no-repeat;
-    background-size: cover;
-    transform: skew(60deg, 0);
-`;
-
-const Info = styled.span`
-    display: flex;
-    margin-bottom: 1.6rem;
-    font-size: 1.6em;
-    font-family: 'Oxygen', sans-serif;
-    color: ${props => props.theme.greyishBrown};
-    vertical-align: middle;
-`;
-const Title = styled.a`
-    margin: 2.4rem 0;
-    font-family: 'Rubik', sans-serif;
-    font-size: 3.6rem;
-    font-weight: bold;
-    text-decoration: none;
-    color: ${props => props.color || props.theme.vividPurpleTwo};
-`;
-
-const Description = styled.p`
-    font-size: 1.4rem;
-    font-family: 'Oxygen', sans-serif;
-    color: ${props => props.theme.greyishBrown};
-`;
-
-const AuthorBadge = styled.div`
-    display: flex;
-    align-items: center;
-    font-size: 1.6rem;
-    font-weight: bold;
-    font-family: 'Oxygen', sans-serif;
-    margin: 2.4rem 0;
-    vertical-align: middle;
-    color: ${props => props.color || props.theme.greyishBrown};
-`;
-const AuthorPhoto = styled.img`
-    width: 3.6rem;
-    height: 3.6rem;
-    border-radius: 50%;
-    margin-right: 1.2rem;
-`;
 const BorderlessButton = styled.button` 
     font-family: Rubik;
 	font-size: 2.4rem;
@@ -129,32 +46,6 @@ const PostsFeed = ({ theme }) => (
             <TagList label="News' tags" tags={tags} />
         )}
         <PostsList>
-            {posts.map((post, postIndex) => (
-                <Post>
-                    <BackgroundShape>
-                        <BackgroundImage url={post.image} />
-                    </BackgroundShape>
-                    <header>
-                        <Info>
-                            {post.original ? <WebpurpleStampIcon style={{ marginRight: '1.6em' }} /> : ''}
-                            <time>{new Date(post.date).toLocaleDateString()}</time>
-                        </Info>
-                        <Info>
-                            <Title color={postIndex % 2 ? theme.vividPurpleTwo : theme.lipstick} href={post.url}>{post.title}</Title>
-                        </Info>
-                    </header>
-                    <Description>
-                        {post.description}
-                    </Description>
-                    <AuthorBadge>
-                        <AuthorPhoto src={post.author.avatar} />
-                        {post.author.displayName}
-                    </AuthorBadge>
-
-                    <TagList tags={post.tags} />
-
-                </Post>
-            ))}
         </PostsList>
     </MainContainer>);
 
---
+++ src/components/feed-page/posts-list.jsx
@@ -0,0 +1,130 @@
+import React from 'react';
+import styled, { withTheme } from 'styled-components';
+
+import BlockHeader from '../common/block-header';
+import { TagList } from '../common/tag';
+import { media } from '../../utils/css-utils';
+import { WebpurpleStampIcon } from '../icons';
+
+import posts, { tags } from './posts-stub';
+
+
+const Container = styled.ul`
+    list-style: none;
+    padding: 0;
+    margin: 3.6rem 0 0;
+    ${media.desktop`margin-top: 10rem;`}
+    display: flex;
+    flex-wrap: wrap;
+    justify-content: space-between;
+`;
+
+const Post = styled.li`
+    position: relative;
+    width: 100%;
+    ${media.tablet`width: 30rem;`}
+    ${media.hd`width: 35rem;`}
+    padding: 2.5rem;
+    margin-bottom: 2rem;
+    ${media.desktop`margin-bottom: 7rem;`}
+    box-sizing: border-box;
+    overflow: hidden;
+    box-shadow: 0 0 8px 1px #bbb;
+`;
+
+const BackgroundShape = styled.div`
+    position: absolute;
+    top: 0;
+    left: -60%;
+    width: 230%;
+    height: 100%;
+    transform: skew(-60deg, 0);
+    overflow: hidden;
+    z-index: -1;
+`;
+
+const BackgroundImage = styled.div`
+    width: 100%;
+    height: 100%;
+    filter: grayscale(100);
+    opacity: .15;
+    background: url(${props => props.url});
+    background-repeat: no-repeat;
+    background-size: cover;
+    transform: skew(60deg, 0);
+`;
+
+const AuthorBadge = styled.div`
+    display: flex;
+    align-items: center;
+    font-size: 1.6rem;
+    font-weight: bold;
+    font-family: 'Oxygen', sans-serif;
+    margin: 2.4rem 0;
+    vertical-align: middle;
+    color: ${props => props.color || props.theme.greyishBrown};
+`;
+const AuthorPhoto = styled.img`
+    width: 3.6rem;
+    height: 3.6rem;
+    border-radius: 50%;
+    margin-right: 1.2rem;
+`;
+
+const Info = styled.span`
+    display: flex;
+    margin-bottom: 1.6rem;
+    font-size: 1.6em;
+    font-family: 'Oxygen', sans-serif;
+    color: ${props => props.theme.greyishBrown};
+    vertical-align: middle;
+`;
+const Title = styled.a`
+    margin: 2.4rem 0;
+    font-family: 'Rubik', sans-serif;
+    font-size: 3.6rem;
+    font-weight: bold;
+    text-decoration: none;
+    color: ${props => props.color || props.theme.vividPurpleTwo};
+`;
+
+const Description = styled.p`
+    font-size: 1.4rem;
+    font-family: 'Oxygen', sans-serif;
+    color: ${props => props.theme.greyishBrown};
+`;
+
+const PostsList = ({ theme }) => (
+    <Container>
+        {posts.map((post, postIndex) => (
+            <Post>
+                <BackgroundShape>
+                    <BackgroundImage url={post.image} />
+                </BackgroundShape>
+                <header>
+                    <Info>
+                        {post.original ? <WebpurpleStampIcon style={{ marginRight: '1.6em' }} /> : ''}
+                        <time>{new Date(post.date).toLocaleDateString()}</time>
+                    </Info>
+                    <Info>
+                        <Title color={postIndex % 2 ? theme.vividPurpleTwo : theme.lipstick} href={post.url}>{post.title}</Title>
+                    </Info>
+                </header>
+                <Description>
+                    {post.description}
+                </Description>
+                <AuthorBadge>
+                    <AuthorPhoto src={post.author.avatar} />
+                    {post.author.displayName}
+                </AuthorBadge>
+                <TagList tags={post.tags} />
+            </Post>
+        ))}
+   </Container>
+);
+
+PostsList.propTypes = {
+    theme: React.PropTypes.object,
+};
+
+export default withTheme(PostsList);
